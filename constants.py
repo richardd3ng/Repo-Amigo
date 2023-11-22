0f5ccd9275cf7d14dfbbba95959bdabdb65dda16
richardd3ng <richard.deng@duke.edu>
2023-11-22 02:34:11 -0500

    test dev mode flag

--- constants.py
+++ constants.py
@@ -1,10 +1,13 @@
+from config import DEV_MODE
+
 # directories
 DATA_ROOT = "./temp_data"
 REPOS_DIR_NAME = "repos"
 DB_DIR_NAME = "db"
 
 # oauth2
-REDIRECT_URI = "http://localhost:8501/github-code"
+BASE_URI = "http://localhost:8501" if DEV_MODE else "https://repo-amigo.streamlit.app"
+REDIRECT_URI = f"{BASE_URI}/github-code"
 GITHUB_AUTHORIZATION_URL = "https://github.com/login/oauth/authorize"
 GITHUB_TOKEN_URL = "https://github.com/login/oauth/access_token"
 GITHUB_USER_URL = "https://api.github.com/user"
